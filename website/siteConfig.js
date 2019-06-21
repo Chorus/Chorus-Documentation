15c4db24dc69c579e36b4fe104caef51f49ce31e
Shaya Gold <sgold@encoresupport.org>
2019-06-21 00:58:06 -0400

    added configs to siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,14 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  title: 'Chorus Documentation', // Title for your website.
+  tagline: 'Chorus Documentation',
+  url: 'https://chorus.github.io', // Your website URL
+  baseUrl: '/Chorus-Documentation/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'Chorus-Documentation',
+  organizationName: 'chorus',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
